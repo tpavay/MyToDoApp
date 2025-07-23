1cd94f21e6f278afa302c408efb9ffac047a4852
tpavay <pavayt@gmail.com>
2025-07-22 20:06:42 -0500

    Initial Commit

---
+++ MyToDoApp/MyToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyToDoAppApp.swift
+//  MyToDoApp
+//
+//  Created by Tyler Pavay on 7/22/25.
+//
+
+import SwiftUI
+
+@main
+struct MyToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
